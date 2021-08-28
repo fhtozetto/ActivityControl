3149e906b2778c1b81af15126ca9ff10d440df44
Fernando Hnerique Tozetto <fhtozetto@gmail.com>
2021-08-28 13:23:12 -0300

    Creating refresh token

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,77 @@
+import { sign, verify } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import auth from '@config/auth';
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { IDateProvider } from '@shared/container/providers/dateProvider/IDateProvider';
+import { AppError } from '@shared/errors/AppError';
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+interface ITokenResponse {
+  token: string;
+  refresh_token: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository,
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+  ) {}
+
+  async execute(token: string): Promise<ITokenResponse> {
+    const {
+      secret_token,
+      secret_refresh_token,
+      expires_in_refresh_token,
+      expires_in_token,
+      expires_refresh_token_days,
+    } = auth;
+
+    const { sub, email } = verify(token, secret_refresh_token) as IPayload;
+
+    const user_id = sub;
+
+    const userToken = await this.usersTokensRepository.findByUserIdAndRefreshToken(
+      user_id,
+      token,
+    );
+
+    if (!userToken) {
+      throw new AppError('Refresh token does not exists!');
+    }
+
+    await this.usersTokensRepository.deleteById(userToken.id);
+
+    const refresh_token = sign({ email }, secret_refresh_token, {
+      subject: user_id,
+      expiresIn: expires_in_refresh_token,
+    });
+
+    const expires_date = this.dateProvider.addDays(expires_refresh_token_days);
+
+    await this.usersTokensRepository.create({
+      expires_date,
+      refresh_token,
+      user_id,
+    });
+
+    const newToken = sign({}, secret_token, {
+      subject: user_id,
+      expiresIn: expires_in_token,
+    });
+
+    return {
+      token: newToken,
+      refresh_token,
+    };
+  }
+}
+
+export { RefreshTokenUseCase };
